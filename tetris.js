7297976c6a30c4d96b7b435a36a32c0b03ffd198
jooseeruu <arjona2908@gmail.com>
2024-11-20 20:59:50 +0100

    piezas

---
+++ tetris.js
@@ -0,0 +1,67 @@
+// Obtener el lienzo y el contexto
+const canvas = document.getElementById("tetris");
+const lienzo = canvas.getContext("2d");
+
+// Constantes del tablero
+const filas = 20;
+const columnas = 10;
+const tamanoCelda = 30;
+
+// Inicializar tablero
+const tablero = Array.from({ length: filas }, () => Array(columnas).fill(0));
+
+// Piezas disponibles
+const piezas = [
+  {
+    nombre: "C",
+    forma: [
+      [1, 1, 1],
+      [1, 0, 1],
+    ],
+    probabilidad: 0.3,
+    color: "red",
+  },
+
+  {
+    nombre: "I",
+    forma: [[1, 1, 1, 1]],
+    probabilidad: 0.1,
+    color: "blue",
+  },
+  {
+    nombre: "L",
+    forma: [
+      [1, 1, 1],
+      [1, 0, 0],
+    ],
+    probabilidad: 0.1,
+    color: "orange",
+  },
+  {
+    nombre: "O",
+    forma: [
+      [1, 1],
+      [1, 1],
+    ],
+    probabilidad: 0.2,
+    color: "yellow",
+  },
+  {
+    nombre: "T",
+    forma: [
+      [1, 1, 1],
+      [0, 1, 0],
+    ],
+    probabilidad: 0.1,
+    color: "purple",
+  },
+  {
+    nombre: "Z",
+    forma: [
+      [1, 1, 0],
+      [0, 1, 1],
+    ],
+    probabilidad: 0.1,
+    color: "cyan",
+  },
+];
